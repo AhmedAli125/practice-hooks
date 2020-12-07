1b7ca70cdeeb40fbf9c2d454d168888e69112c04
AhmedAli125 <ali405526@gmail.com>
2020-12-08 02:15:48 +0500

    weather app using hooks and weatherAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import GetPosts from './components/HttpComponent/GetPosts';
-import UseStateForm from './components/hooksComponents/UseStateForm';
-import UseStateArray from './components/hooksComponents/UseStateArray';
-import CondtionalEffect from './components/hooksComponents/ConditionalEffect';
-import ConditionalEffect from './components/hooksComponents/ConditionalEffect';
-import FetchDataHook from './components/hooksComponents/FetchDataHook'
-
-
-import FirstLevel from './components/hooksComponents/useContext/FirstLevel'
-
-import UserContext from './components/hooksComponents/useContext/UserContext'   //new code
-
-
-import './App.css'
-
-
-
-// export const UserContext = React.createContext()
+// import GetPosts from './components/HttpComponent/GetPosts';
+// import UseStateForm from './components/hooksComponents/UseStateForm';
+// import UseStateArray from './components/hooksComponents/UseStateArray';
+// import CondtionalEffect from './components/hooksComponents/ConditionalEffect';
+// import ConditionalEffect from './components/hooksComponents/ConditionalEffect';
+// import FetchDataHook from './components/hooksComponents/FetchDataHook'
+// import FirstLevel from './components/hooksComponents/useContext/FirstLevel'
+// import UserContext from './components/hooksComponents/useContext/UserContext'   //new code
+import WeatherAppContainer from './components/weather-app/WeatherAppContainer'
+// import './App.css'
 function App() {
+// export const UserContext = React.createContext()
 
-  const [userName, setUserName] = React.useState('')
+  // const [userName, setUserName] = React.useState('')
   // console.log(UserContext);
 
   return (
@@ -40,7 +33,7 @@ function App() {
         <FirstLevel />
       </UserContext.Provider> */}
 
-
+      <WeatherAppContainer />
     </div>
   );
 }
